17cdf2fd8ded0d36d2ef7253dc6faacadcde7db5
CckRocket <cck_rocket@163.com>
2020-05-31 13:02:40 +0800

    mergesort: reduce space overhead

--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -4,22 +4,22 @@ import java.util.Arrays;
 
 public class MergeSort {
 	public static void sort(int[] arr) {
-		sort(arr, 0, arr.length-1);
+		int[] tempArr = new int[arr.length];
+		sort(arr, tempArr, 0, arr.length-1);
 		System.out.print("Merge sort:  " + Arrays.toString(arr));
 		System.out.println();
 	}
-	private static void sort(int[] arr, int start, int end) {
+	private static void sort(int[] arr, int[] tempArr, int start, int end) {
 		if(start >= end) {
 			return;
 		}
 		int mid = (start + end) / 2;
-		sort(arr, start, mid);
-		sort(arr, mid+1, end);
-		merge(arr, start, mid, end);
+		sort(arr, tempArr, start, mid);
+		sort(arr, tempArr, mid+1, end);
+		merge(arr, tempArr, start, mid, end);
 	}
-	private static void merge(int[] arr, int start, int mid, int end) {
+	private static void merge(int[] arr, int[] tempArr, int start, int mid, int end) {
 		int len = end - start + 1;
-		int[] tempArr = new int[len];
 		int tempIndex = 0;
 		int left = start;
 		int right = mid + 1;
